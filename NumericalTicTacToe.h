32dddb0c0e58a167c5bb40a4963e9166c0451fae
MALAK <11410120230419@stud.cu.edu.eg>
2024-12-03 23:45:55 +0200

    Add Connect-4 by XO game

--- NumericalTicTacToe.h
+++ NumericalTicTacToe.h
@@ -2,15 +2,15 @@
 // Created by MALAK on 12/3/2024.
 //
 
-#ifndef CONNECTFOUR_H
-#define CONNECTFOUR_H
+#ifndef NUMERICALTICTACTOE_H
+#define NUMERICALTICTACTOE_H
 
 #include "BoardGame_Classes.h"
 
 template <typename T>
-class ConnectFourBoard : public Board<T> {
+class NumericalTicTacToe_Board : public Board<T> {
 public:
-    ConnectFourBoard ();
+    NumericalTicTacToe_Board ();
     bool update_board (int x , int y , T symbol) override;
     void display_board () override;
     bool is_win() override;
@@ -19,16 +19,16 @@ public:
 };
 
 template <typename T>
-class ConnectFourPlayer : public Player<T> {
-    public:
-    ConnectFourPlayer (string name , T symbol);
+class NumericalTicTacToe_Player : public Player<T> {
+public:
+    NumericalTicTacToe_Player (string name , T symbol);
     void getmove(int& x, int& y) override;
 };
 
 template <typename T>
-class ConnectFourRandomPlayer : public RandomPlayer<T> {
-    public:
-    ConnectFourRandomPlayer (T symbol);
+class NumericalTicTacToe_RandomPlayer : public RandomPlayer<T> {
+public:
+    NumericalTicTacToe_RandomPlayer (T symbol);
     void getmove(int& x, int& y) override;
 };
 
@@ -37,47 +37,52 @@ class ConnectFourRandomPlayer : public RandomPlayer<T> {
 #include <bits/stdc++.h>
 using namespace std;
 
-//Constructor for connect 4 board
+//Constructor for NumericalTicTacToe board
 template<typename T>
-ConnectFourBoard<T>::ConnectFourBoard() {
-    this -> columns = 7 ;
-    this -> rows = 6 ;
-    this->board = new char*[this->rows];
+NumericalTicTacToe_Board <T>::NumericalTicTacToe_Board () {
+    this -> columns = 3 ;
+    this -> rows = 3 ;
+    this->board = new int*[this->rows];
     for (int i = 0; i < this->rows; i++) {
-        this->board[i] = new char[this->columns];
+        this->board[i] = new int[this->columns];
         for (int j = 0; j < this->columns; j++) {
-            this->board[i][j] = ' ';
+            this->board[i][j] = 0 ;
         }
     }
     this->n_moves = 0 ;
 }
 
 template<typename T>
-bool ConnectFourBoard<T>::update_board(int x, int y, T symbol) {
-    if ( x < 0 || x >= this->rows || y < 0 || y >= this->columns || this->board[x][y] != ' ') {
+bool NumericalTicTacToe_Board<T>::update_board(int x, int y, T number) {
+    vector<int>keepTrack(9 , 0);
+    if ( x < 0 || x >= this->rows || y < 0 || y >= this->columns || this->board[x][y] != 0) {
         cout << "Invalid input" << endl;
         return false;
     }
-
-    for (int i = this -> rows - 1 ; i >= 0; --i) {//Must start from the bottom so for the fiven col chechk the rows from down
-        if(this->board[i][y] == ' ') {
-            if (x != i ) {
-                return false;
-            }
-            if (x == i) {
-                this->board[x][y] = toupper(symbol);
-                ++this->n_moves;
-                return true;
-            }
+    if(keepTrack[number] > 1) { //numbers are only used once
+        return false;
+    }
+    if(this->n_moves % 2 == 0) {
+        if(number % 2 == 0) {
+            cout << "First Player Must choose an ood number \n";
+            return false;
         }
     }
-    this->board[x][y] = toupper(symbol);
+    if(this->n_moves % 2 == 1) {
+        if(number % 2 != 0) {
+            cout << "Second Player Must choose an even number \n";
+            return false;
+        }
+    }
+
+    this->board[x][y] = number;
+    ++keepTrack[number - 1];
     ++this->n_moves;
     return true;
 }
 
 template<typename T>
-void ConnectFourBoard<T>::display_board() {
+void NumericalTicTacToe_Board<T>::display_board() {
     for (int i = 0; i < this->rows; i++) {
         cout << "\n|";
         for (int j = 0; j < this->columns; j++) {
@@ -88,20 +93,19 @@ void ConnectFourBoard<T>::display_board() {
     cout << endl;
 }
 
-
 template<typename T>
-bool ConnectFourBoard<T>::is_win() {
-//first player to get four-in-a-row vertically, horizontally, or diagonally wins.
+bool NumericalTicTacToe_Board<T>::is_win() {
+//Winning: A player wins by placing three numbers in a row, column, or diagonal that add up to 15.
 
-    //Diagonals for O
+    //Diagonal
     for (int i = 0; i < this->rows; i++) {
         for (int j = 0; j < this->columns; j++) {
-            if(i - 4 >= 0 && j + 4 < this->columns) {
-                if (this->board[i][j] == 'O' && this->board[i - 1 ][j + 1] == 'O' && this->board[i - 2 ][j + 2] == 'O' && this->board[i - 3][j + 3] == 'O') {
+            if(i - 2 >= 0 && j + 2 < this->columns) {
+                if (this->board[i][j] + this->board[i - 1 ][j + 1] + this->board[i - 2 ][j + 2] == 15) {
                     return true;
                 }
             }
-            else if(i + 4 < this->rows && j + 4 >= 0) {
+            else if(i + 4 < this->rows && j - 4 >= 0) {
                 if (this->board[i][j] == 'O' && this->board[i + 1 ][j + 1] == 'O' && this->board[i + 2 ][j + 2] == 'O' && this->board[i + 3][j + 3] == 'O') {
                     return true;
                 }
@@ -117,7 +121,7 @@ bool ConnectFourBoard<T>::is_win() {
                     return true;
                 }
             }
-            else if(i + 4 < this->rows && j + 4 >= 0) {
+            else if(i + 4 < this->rows && j - 4 >= 0) {
                 if (this->board[i][j] == 'X' && this->board[i + 1 ][j + 1] == 'X' && this->board[i + 2 ][j + 2] == 'X' && this->board[i + 3][j + 3] == 'X') {
                     return true;
                 }
@@ -128,7 +132,7 @@ bool ConnectFourBoard<T>::is_win() {
     //Horizontal O - Next to each other
     for (int i = 0; i < this->columns; i++) {
         for (int j = 0; j < this->rows; j++) {
-            if(i + 3 < this->columns ) {
+            if(i + 3 <= this->columns - 1) {
                 if (this->board[j][i] == 'O' && this->board[j][i + 1] == 'O' && this->board[j][i + 2] == 'O' && this->board[j][i + 3] == 'O') {
                     return true;
                 }
@@ -150,7 +154,7 @@ bool ConnectFourBoard<T>::is_win() {
     //Vertical X - Next to each other
     for (int i = 0; i < this->columns; i++) {
         for (int j = 0; j < this->rows; j++) {
-            if(j + 3 < this->rows ) {
+            if(j + 3 <= this->rows - 1) {
                 if (this->board[j][i] == 'X' && this->board[j + 1 ][i] == 'X' && this->board[j + 2 ][i] == 'X' && this->board[j + 3 ][i] == 'X') {
                     return true;
                 }
@@ -161,7 +165,7 @@ bool ConnectFourBoard<T>::is_win() {
     //Vertical O - Next to each other
     for (int i = 0; i < this->columns; i++) {
         for (int j = 0; j < this->rows; j++) {
-            if(j + 3 < this->rows ) {
+            if(j + 3 <= this->rows - 1) {
                 if (this->board[j][i] == 'O' && this->board[j + 1 ][i] == 'O' && this->board[j + 2 ][i] == 'O' && this->board[j + 3 ][i] == 'O') {
                     return true;
                 }
@@ -183,23 +187,5 @@ bool ConnectFourBoard<T>::game_is_over() {
 //--------------------------------------------------------------------------------------
 
 // Constructor for connect 4 player
-template<typename T>
-ConnectFourPlayer<T>::ConnectFourPlayer(string name, T symbol) : Player<T>(name, symbol){}
 
-template<typename T>
-void ConnectFourPlayer<T>::getmove(int &x, int &y) {
-    cout << "Please enter the coordinates of the move : (e.g. 0 2)\n";
-    cin >> x;
-    cin >> y;
-    cin.ignore();
-}
-template<typename T>
-ConnectFourRandomPlayer<T>::ConnectFourRandomPlayer(T symbol) : RandomPlayer<T>(symbol){}
-
-template<typename T>
-void ConnectFourRandomPlayer<T>::getmove(int &x, int &y) {
-    x = rand() % 6;  // Random number between 0 and 5
-    y = rand() % 7;  // Random number between 0 and 6
-}
-
-#endif //CONNECTFOUR_H
+#endif //NUMERICALTICTACTOE_H
